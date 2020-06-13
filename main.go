a55acee75994315fe9a7eb406f0967dc908c2425
Petru <smart__petea@mail.ru>
2020-06-13 12:40:14 +0300

    RoutingEntryInterface

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
     "log"
     "os"
     "encoding/json"
+    "regexp"
 )
 
 func main() {
@@ -25,27 +26,70 @@ func main() {
     }
 
     gormAutoMigrate(db)
-    configureHttpEndpoints(db)
 
-    err = runServer()
+    err = runServer(db)
     if err != nil {
         log.Fatal()
     }
 }
 
-func runServer() error {
+type RoutingEntryInterface interface {
+    run(w http.ResponseWriter, r *http.Request, urlMatches map[string]string, db *gorm.DB)
+}
+
+func runServer(db *gorm.DB) error {
     serverPort := os.Getenv("SERVER_PORT")
     serverUrl := fmt.Sprintf("127.0.0.1:%s", serverPort)
     log.Printf("Server started at %s", serverUrl)
 
+    routing := make(map[string]map[*regexp.Regexp]RoutingEntryInterface)
+    routing[http.MethodGet] = make(map[*regexp.Regexp]RoutingEntryInterface)
+    routing[http.MethodPut] = make(map[*regexp.Regexp]RoutingEntryInterface)
+    routing[http.MethodPost] = make(map[*regexp.Regexp]RoutingEntryInterface)
+    routing[http.MethodDelete] = make(map[*regexp.Regexp]RoutingEntryInterface)
+
+    categoryRegex := regexp.MustCompile(`^/category$`)
+    routing[http.MethodGet][categoryRegex] = &CategoryGetAll{}
+    routing[http.MethodPost][categoryRegex] = &CategoryPost{}
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        log.Printf("%s %s", r.Method, r.URL.Path)
+        _, ok := routing[r.Method]
+        if !ok {
+            http.Error(w, "Not a supported http method", 500)
+            return 
+        }
+
+        for routingRegex, routingEntry := range routing[r.Method] {
+            match := routingRegex.FindStringSubmatch(r.URL.Path)
+            if len(match) == 0 {
+                continue
+            }
+
+            result := make(map[string]string)
+            for i, name := range routingRegex.SubexpNames() {
+                if i != 0 && name != "" {
+                    result[name] = match[i]
+                }
+            }
+
+            routingEntry.run(w, r, result, db)
+            return
+        }
+
+        http.Error(w, "no suitable route found", 400)
+    })
+
     return http.ListenAndServe(serverUrl, nil)
 }
 
+/*
 func configureHttpEndpoints(db *gorm.DB) {
     baseHandler := NewBaseHandler(db)
 
-    http.HandleFunc("/category", baseHandler.getCategories)
+    http.HandleFunc("/", baseHandler.getCategories)
 }
+*/
 
 func connectPostgres() (db *gorm.DB, err error) {
     postgresUrl := fmt.Sprintf(
@@ -84,66 +128,60 @@ func gormAutoMigrate(db *gorm.DB) {
     db.AutoMigrate(&Good{})
 }
 
-type BaseHandler struct {
-    db *gorm.DB
-}
+type CategoryGetAll struct {}
+func (*CategoryGetAll) run(
+    w http.ResponseWriter,
+    r *http.Request,
+    urlMatches map[string]string,
+    db *gorm.DB,
+) {
+    categories := []Category{}
+    db.Find(&categories)
 
-func NewBaseHandler(db *gorm.DB) *BaseHandler {
-    return &BaseHandler{
-        db: db,
+    obj, err := json.Marshal(categories)
+    if err != nil {
+        //add log
+        http.Error(w, "error at json marshalling", 500)
+        return
+    } else {
+        w.Write(obj)
     }
 }
 
-func (h *BaseHandler) getCategories(w http.ResponseWriter, r *http.Request) {
-    switch r.Method {
-        case http.MethodGet:
-            categories := []Category{}
-            h.db.Find(&categories)
-
-            obj, err := json.Marshal(categories)
-            if err != nil {
-                //add log
-                http.Error(w, "error at json marshalling", 500)
-                return
-            } else {
-                w.Write(obj)
-            }
-
-        case http.MethodPost:
-            if r.Body == nil {
-                http.Error(w, "Please send a request body", 400)
-                log.Println("Empty body")
-                return
-            }
-
-            var category Category
-            err := json.NewDecoder(r.Body).Decode(&category)
-            if err != nil {
-                log.Println("Error: ", err)
-                http.Error(w, err.Error(), 400)
-                return
-            }
-
-            err = h.db.Create(&category).Error
-            if err != nil {
-                log.Println("Error: ", err)
-                http.Error(w, err.Error(), 400)
-                return
-            }
-
-            categoryJson, err := json.Marshal(category)
-            if err != nil {
-                log.Println("Error: ", err)
-                http.Error(w, err.Error(), 400)
-                return
-            }
-
-            w.Write(categoryJson)
-
-        case http.MethodDelete:
-        case http.MethodPut:
-        default:
-
+type CategoryPost struct {}
+func (*CategoryPost) run(
+    w http.ResponseWriter,
+    r *http.Request,
+    urlMatches map[string]string,
+    db *gorm.DB,
+) {
+    if r.Body == nil {
+        http.Error(w, "Please send a request body", 400)
+        log.Println("Empty body")
+        return
     }
 
+    var category Category
+    err := json.NewDecoder(r.Body).Decode(&category)
+    if err != nil {
+        log.Println("Error: ", err)
+        http.Error(w, err.Error(), 400)
+        return
+    }
+
+    err = db.Create(&category).Error
+    if err != nil {
+        log.Println("Error: ", err)
+        http.Error(w, err.Error(), 400)
+        return
+    }
+
+    categoryJson, err := json.Marshal(category)
+    if err != nil {
+        log.Println("Error: ", err)
+        http.Error(w, err.Error(), 400)
+        return
+    }
+
+    w.Write(categoryJson)
 }
